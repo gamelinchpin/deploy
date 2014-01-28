f7a0f428315191168a54d5773dfdcd8df71212fa
Simon Hildrew <simon.hildrew@guardian.co.uk>
2014-01-28 17:31:18 +0000

    Cap the output of command line tools

--- magenta-lib/src/main/scala/magenta/tasks/CommandLine.scala
+++ magenta-lib/src/main/scala/magenta/tasks/CommandLine.scala
@@ -4,7 +4,7 @@ package tasks
 // represents a command line to execute
 //  including the ability to actually execute and become remote
 case class CommandLine(commandLine: List[String], successCodes: List[Int] = List(0)) {
-  lazy val quoted = (commandLine) map quoteIfNeeded mkString " "
+  lazy val quoted = commandLine map quoteIfNeeded mkString " "
   private def quoteIfNeeded(s: String) = if (s.contains(" ")) "\"" + s + "\"" else s
 
   def suppressor(filteredOut: String => Unit) = { line:String =>
@@ -13,14 +13,31 @@ case class CommandLine(commandLine: List[String], successCodes: List[Int] = List
         filteredOut(line)
   }
 
+  def capAt(cap: Int, filteredOut: String => Unit): String => Unit = new CapAt(cap, filteredOut).processLine
+
   def run() {
     import sys.process._
     MessageBroker.infoContext("$ " + quoted) {
-      val returnValue = commandLine ! ProcessLogger(MessageBroker.commandOutput(_), suppressor(MessageBroker.commandError(_)))
+      val returnValue = commandLine ! ProcessLogger(
+        capAt(250, MessageBroker.commandOutput),
+        suppressor(capAt(250, MessageBroker.commandError))
+      )
       MessageBroker.verbose("return value " + returnValue)
       if (!successCodes.contains(returnValue)) {
-        MessageBroker.fail("Exit code %d from command: %s" format (returnValue, quoted))
+        MessageBroker.fail(s"Exit code $returnValue from command: $quoted")
       }
+    }
+  }
+}
+
+class CapAt(capAt:Int, forwarder: String => Unit) {
+  var counter = 0
+  def processLine(line:String) {
+    counter = counter+1
+    counter match {
+      case normal if normal < capAt => forwarder(line)
+      case notify if notify % capAt == 0 => forwarder(s"WARNING: Too many lines output ($counter) ignoring output")
+      case _ =>
     }
   }
 }
