ccf7759475b8c6d4cc3ad5f535857631da6e49a2
Simon Hildrew <simon.hildrew@guardian.co.uk>
2014-03-31 17:08:07 +0100

    Polishing

--- riff-raff/app/utils/ChargingUpdate.scala
+++ riff-raff/app/utils/ChargingUpdate.scala
@@ -5,14 +5,15 @@ import play.api.mvc.RequestHeader
 
 object ChargingUpdate {
   lazy val interval = new Interval(
-    new DateTime(2014,4,1,0,0,0), new DateTime(2014,4,1,13,0,0)
+    new DateTime(2014,4,1,0,0,0), new DateTime(2014,4,1,12,0,0)
   )
 
   def choose[A](normal: A)(inInterval: A):A =
     if (interval.contains(new DateTime())) inInterval else normal
 
-  def visible(implicit request:RequestHeader) = {
-    request.session.get("seenChargeDialog").isEmpty
-    //choose(false)(true)
+  def visible = {
+    choose(false)(true)
   }
+
+  def bannerSeen(implicit request:RequestHeader) = request.session.get("seenChargeDialog").isDefined
 }
